9bbca46c06c0151f6f0bc465ca96120749fd5b67
Sami Chebbi <schebbi@cern.ch>
2021-02-23 10:59:01 +0100

    whodidwhat integration to puppet

--- whodidwhat.pp
+++ whodidwhat.pp
@@ -2,9 +2,21 @@ class hg_box::monitor{
   
 
   #whodidwhat Scripts
-  file {'cernbox/ops':
-      ensure => 'file',
-      owner => 'root',
-      mode  => '755', 
-      recurse => true,
-      source => 'puppet:///modules/hg_box/monitor/whodidwhat',
+  file {'/usr/local/sbin/whodidwhat':
+      ensure  => 'present'
+      owner   => 'root',
+      mode    => '755', 
+      source  => 'puppet:///modules/hg_box/monitor/cbox-op-scripts/whodidwhat.sh',
+
+  }
+
+  file {'/etc/profile.d/whodidwhat.sh':
+      ensure  => 'present'
+      owner   => 'root',
+      mode    => '755', 
+      source  => 'puppet:///modules/hg_box/monitor/cbox-op-scripts/whodidwhat.profile.sh',
+
+  }
+
+
+
