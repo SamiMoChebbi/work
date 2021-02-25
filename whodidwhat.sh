4c19ed1668123108375f4445d698e53e04d3bead
GunaSami <78431212+SamiMoChebbi@users.noreply.github.com>
2021-02-25 15:12:30 +0100

    Update of the whodidwhat

--- whodidwhat.sh
+++ whodidwhat.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/awk -f
+#!/usr/bin/bash
+
+
+tail -n +1 ~/.bash_history_* | awk '
+/==>/{
+    split($2,a,"_")
+    user=a[length(a)]
+    next
+}
 
 /#[0-9]+/{
     printf "%s", strftime("%Y-%m-%d %H:%M:%S", substr($1, 2, length($1)))
 }
 
 !/#[0-9]+/{
-    print " ", $1
+    print " "user," ", $0
 }
+' | sort
