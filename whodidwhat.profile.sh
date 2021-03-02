34404255bea828ff56fa830b248119b879bacb49
GunaSami <78431212+SamiMoChebbi@users.noreply.github.com>
2021-03-02 15:28:18 +0100

    Update whodidwhat.profile.sh

--- whodidwhat.profile.sh
+++ whodidwhat.profile.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 export LOGDINUSER=`klist | grep "Default principal" | awk '{print $3}' | sed 's/@CERN.CH//'`
 export HISTTIMEFORMAT="@ %m/%d/%Y %H:%M "
 export HISTFILE=~/.bash_history_$LOGDINUSER
